7590c472a934c0820d544c1424c12ca48156e36b
m2web <m2web@yahoo.com>
2018-06-12 20:34:25 -0400

    version updates for the package.json and lock as well as first lesson.

---
+++ examples/001/hello.js
@@ -0,0 +1,10 @@
+// console.log(process.argv);
+const args = process.argv.slice(2);
+const [name] = args;
+
+if(name === undefined){
+    console.error('Please pass a name, e.g. node hello.js Mark');
+    process.exit(0);
+}
+
+console.log(`Good day to you, ${name}`);
