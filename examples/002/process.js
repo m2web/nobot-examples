3ede425cc940cf0ed4c92466b7ca21d4580ba2fd
m2web <m2web@yahoo.com>
2018-06-13 19:13:03 -0400

    Added ESLint w/ AirBnb styles.

--- examples/002/process.js
+++ examples/002/process.js
@@ -11,13 +11,14 @@ process.stdout.write('Type something then hit enter: \n');
 process.stdin.setEncoding('utf8');
 
 process.stdin.on('readable', () => {
-    const chunk = process.stdin.read();
-    if (chunk !== null){
-        process.stdout.write(`You wrote: ${chunk}`);
-        process.exit(0);
-    }
+  const chunk = process.stdin.read();
+  if (chunk !== null) {
+    process.stdout.write(`You wrote: ${chunk}`);
+    process.exit(0);
+  }
 });
 
 process.on('exit', (code) => {
-    console.log(`This process has now finished, exiting with code: ${code}`);
+  console.log(`This process has now finished, exiting with code: ${code}`);
 });
+
