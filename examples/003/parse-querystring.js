a1d3583ae4df49cb25bee5e1c84463452d62d9c8
m2web <m2web@yahoo.com>
2018-06-14 16:59:13 -0400

    Completed section 003

--- examples/003/parse-querystring.js
+++ examples/003/parse-querystring.js
@@ -1,1 +1,8 @@
 const querystring = require('querystring');
+
+const url = 'https://someurl.com?myName=Mark&myAge=57&myComment=yes+I+am+old';
+const parsedUrl = querystring.parse(url.substr(url.indexOf('?') + 1));
+
+console.log(`Hello my name is: ${parsedUrl.myName}`);
+console.log(`My age: ${parsedUrl.myAge}`);
+console.log(`Oh and ${parsedUrl.myComment}`);
