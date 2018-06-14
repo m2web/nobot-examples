4a107e9f71177d68b1d2b49e6907db4b45d110a5
m2web <m2web@yahoo.com>
2018-06-14 07:26:03 -0400

    Added babel-eslint to the eslintrc

--- examples/003/build-querystring.js
+++ examples/003/build-querystring.js
@@ -1,1 +1,13 @@
 const querystring = require('querystring');
+
+const apiHost = 'https://jira.mycompany.com/rest/api/latest/searchable?jql=';
+
+const jqlParams = {
+  assignee: 'Mark McFadden',
+  startAt: 2,
+  maxResults: 2
+};
+
+const apiUrl = `${apiHost}"${querystring.stringify(jqlParams)}"`;
+
+console.log(`My JQL api call is ${apiUrl}`);
