86c1b0c696989b1724ff463de17ca7dd445ffcbc
m2web <m2web@yahoo.com>
2018-06-14 20:31:33 -0400

    Completed 004

--- examples/004/url.js
+++ examples/004/url.js
@@ -1,1 +1,19 @@
 const url = require('url');
+
+const args = process.argv.slice(2);
+const [urlEntered] = args;
+
+if (urlEntered === undefined) {
+  console.log('Please pass a URL e.g. https://search.com/?q=mowanna');
+  process.exit(0);
+}
+
+const {
+  protocol, slashes, host, query, href
+} = url.parse(urlEntered);
+
+console.log(`Using protocol: ${protocol}`);
+console.log(`Using slashes: ${slashes}`);
+console.log(`Using host: ${host}`);
+console.log(`Using query: ${query}`);
+console.log(`Using href: ${href}`);
