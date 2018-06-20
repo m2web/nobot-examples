3309bc830e907040bbd4e5d2a485af8da38aeffc
m2web <m2web@yahoo.com>
2018-06-20 19:48:55 -0400

    Started 007

---
+++ examples/007/read-json.js
@@ -0,0 +1,10 @@
+const { projectId, startDate, endDate } = require('./data/example-config');
+
+console.log(`The ID of the project is: ${projectId}`);
+console.log(`The start date of the project is: ${startDate}`);
+console.log(`The end date of the project is: ${endDate}`);
+
+const { name, address } = require('./data/test');
+
+console.log(`The name is: ${name}`);
+console.log(`The address is: ${address}`);
